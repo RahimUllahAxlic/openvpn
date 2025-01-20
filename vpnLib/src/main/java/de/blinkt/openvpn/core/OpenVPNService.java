c18f90a60c480ef4dbacee646c9e89fbc81f8bfe
Rahimullah42 <geeky.rahim@gmail.com>
2025-01-20 17:02:17 +0500

    text of notification change

--- vpnLib/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
+++ vpnLib/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
@@ -456,15 +456,6 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
                 getString(R.string.cancel_connection),
                 openAppPendingIntent
         );
-//             /// this is open app button
-//         Intent openAppIntent = new Intent();
-//         openAppIntent.setClassName(getPackageName(), "net.cipherhaven.vpn.main");  // Replace with your actual MainActivity path
-//         openAppIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//
-//         PendingIntent openAppPendingIntent = PendingIntent.getActivity(this, 0, openAppIntent, PendingIntent.FLAG_IMMUTABLE);
-//
-//         nbuilder.addAction(R.drawable.ic_menu_close_clear_cancel,
-//                 getString(R.string.cancel_connection), openAppPendingIntent);
 
 // this is disconnect button
 
